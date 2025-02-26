7369aec72c419207c9cf204db35f1d28ebe12663
esaLCuE <louischenot@laposte.net>
2025-02-26 14:18:34 +0100

    Update AppWebApplicationTests.java

--- src/test/java/fr/afpa/pompey/cda/appweb/AppWebApplicationTests.java
+++ src/test/java/fr/afpa/pompey/cda/appweb/AppWebApplicationTests.java
@@ -3,7 +3,6 @@ package fr.afpa.pompey.cda.appweb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
 @SpringBootTest
 class AppWebApplicationTests {
 
